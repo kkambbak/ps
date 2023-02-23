f9994550fbf36d081fff5f9d54c456609b5c8c49
kkambbak <yunsung9841@gmail.com>
2023-02-23 09:28:08 +0900

    230223

---
+++ src/main/java/com/ll/level0/p120807/Main.java
@@ -0,0 +1,14 @@
+package com.ll.level0.p120807;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1==num2 ? 1 : -1;
+
+        return answer;
+    }
+}
