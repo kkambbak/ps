6915ea2382cbc5a53a72c6232969e8938f99d252
kkambbak <yunsung9841@gmail.com>
2023-03-13 21:55:58 +0900

    p120897 숫자찾기

---
+++ src/main/java/com/ll/level0/p120904/main.java
@@ -0,0 +1,20 @@
+package com.ll.level0.p120904;
+
+public class main {
+    public static void main(String[] args) {
+        new Solution().solution(29183, 1);
+    }
+}
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        int len = (num+"").length();
+        for (int i = 0; i < len; i++) {
+            if(num%10 == k)
+                answer = len-i;
+            num/=10;
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
