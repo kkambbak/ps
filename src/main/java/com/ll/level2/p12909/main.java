06872d0c89e59f28b47f511cea61f8591e7e8963
kkambbak <yunsung9841@gmail.com>
2023-03-20 22:57:24 +0900

    12909  괄호

--- src/main/java/com/ll/level2/p12909/main.java
+++ src/main/java/com/ll/level2/p12909/main.java
@@ -1,4 +1,28 @@
 package com.ll.level2.p12909;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
 public class main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution());
+    }
+}
+class Solution{
+
+    boolean solution() {
+        String s = "()(())";
+
+        int idx =0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)==('(')){
+                 idx++;
+            }
+            else if(s.charAt(i)==(')')&&idx==0){
+                return false;
+            } else idx--;
+        }
+        return idx==0;
+    }
 }
