887d67092f935b856b28555a99b22386db6e4dd9
kkambbak <71304637+kkambbak@users.noreply.github.com>
2024-02-14 10:47:16 +0900

    [level 1] Title: 옹알이 (2), Time: 4.22 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String s: babbling){
+            s=s.replaceAll("ayaaya","!");
+            s=s.replaceAll("yeye","!");
+            s=s.replaceAll("woowoo","!");
+            s=s.replaceAll("mama","!");            
+            
+            s=s.replaceAll("aya","*");
+            s=s.replaceAll("ye","*");
+            s=s.replaceAll("woo","*");
+            s=s.replaceAll("ma","*");
+            StringBuilder sb= new StringBuilder();
+            for(int i = 0; i < s.length(); i++){
+                sb.append("*");
+            }
+            if(s.equals(sb.toString())) answer++;
+        }
+        return answer;
+    }
+}
